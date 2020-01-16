47ccf44750379684bc4ae4e0b895e7279e3f5010
Aaron Andersen <anderseana@tutanota.com>
2020-01-16 14:46:03 -0600

    got rid of un-needed code

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -1,39 +1,24 @@
 import React, {useState} from "react";
 import Display from "./components/Display";
 import DashBoard from "./components/DashBoard";
-import "./App.css";
 
 function App() {
 const [homeScore, setHomeScore] = useState(0);
 const [awayScore, setAwayScore] = useState(0);
 
   return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Packers</h2>
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">CowBoys</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
         <Display />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
-        </div>
-      </section>
-    </div>
+      
+      // <section className="buttons">
+      //   <div className="homeButtons">
+      //     <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
+      //     <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
+      //   </div>
+      //   <div className="awayButtons">
+      //     <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
+      //     <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
+      //   </div>
+      // </section>
   );
 }
 
